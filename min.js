b75550690c5fe43a9d2d303229dd2f487d543000
khabab <khabbab50@gmail.com>
2023-12-30 16:46:04 +0600

    javaScript somall project no 6

---
+++ min.js
@@ -0,0 +1,15 @@
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const api_url = "https://api.quotable.io/random";
+
+async function getquote(url) {
+    const response = await fetch(url);
+    var data = await response.json();
+    quote.innerHTML = data.content;
+    author.innerHTML = data.author;
+}
+getquote(api_url);
+
+function tweet() {
+    window.open("https://twitter.com/intent/tweet?text=Hello%20world");
+}
